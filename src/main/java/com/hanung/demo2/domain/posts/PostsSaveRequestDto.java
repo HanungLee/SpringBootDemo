9b0511bd9d599e44791753f6f249927409fc915f
hanung <woong102345@gmail.com>
2018-09-18 18:43:27 +0900

    error with junit test

---
+++ src/main/java/com/hanung/demo2/domain/posts/PostsSaveRequestDto.java
@@ -0,0 +1,22 @@
+package com.hanung.demo2.domain.posts;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
--- src/test/java/com/hanung/demo2/Demo2ApplicationTests.java
+++ src/test/java/com/hanung/demo2/Demo2ApplicationTests.java
@@ -1,8 +1,11 @@
 package com.hanung.demo2;
 
+import com.hanung.demo2.domain.posts.PostsRepositoryTest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
